c494a078a03e9ec5976a4e0be5ebb3910e8e3a49
amarcolin <39105551+amarcolin@users.noreply.github.com>
2022-11-14 16:48:17 -0800

    pwa

--- js/script.js
+++ js/script.js
@@ -649,3 +649,16 @@ var sliders = document.getElementsByClassName("slider");
 input.focus();
 updateEnabled();
 
+
+
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
+
